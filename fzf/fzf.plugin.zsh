f3ef6a732e09a5dcfcab05343e5c11d0c861f6a2
Gigitsu <gigitsu.23@gmail.com>
2022-08-04 17:23:31 +0200

    Added custom fzf configration

--- fzf/fzf.plugin.zsh
+++ fzf/fzf.plugin.zsh
@@ -9,3 +9,11 @@ if (( ${+commands[brew]} )); then
 
   unset _fzf_path
 fi
+
+if (( $+commands[rg] )); then
+  export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --no-ignore-vcs -g "!{node_modules,.git}"'
+  export FZF_CTRL_T_COMMAND='rg --files --hidden --follow -g "!{node_modules,.git}"'
+fi
+
+export FZF_DEFAULT_OPTS='--height 96% --reverse'
+export FZF_CTRL_T_OPTS="$FZF_DEFAULT_OPTS --preview \"cat {}\""
