0fbcd017e4bf113d271d30bbfaff9a9c4b019a92
Gigitsu <gigitsu.23@gmail.com>
2022-08-28 16:01:30 +0200

    Fixed nested sessions bug

--- tmux/helpers/open_session.sh
+++ tmux/helpers/open_session.sh
@@ -4,15 +4,11 @@ session_exists() {
   tmux has-session -t "=$1"
 }
 
-not_in_tmux() {
-  [ -z "$TMUX" ]
-}
-
 open_session() {
   local session_path=$1
   local session_name="$(basename "$session_path" | tr . -)"
 
-  if not_in_tmux; then
+  if [ -z "$TMUX" ]; then
     tmux new-session -As "$session_name"
   else
     if ! session_exists "$session_name"; then
--- tmux/tmux.plugin.zsh
+++ tmux/tmux.plugin.zsh
@@ -4,4 +4,6 @@ if [[ ! -e "$HOME/.tmux.conf" ]]; then
   ln -s $root/tmux.conf $HOME/.tmux.conf
 fi
 
-$root/helpers/open_session.sh $PWD
+if [ -z "$TMUX" ]; then
+  $root/helpers/open_session.sh $PWD
+fi
