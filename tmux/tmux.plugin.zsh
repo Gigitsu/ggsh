64c7d9c11452c9f811cfd4c0bd90d0282fe267dc
Gigitsu <gigitsu.23@gmail.com>
2022-12-13 18:31:45 +0100

    Removed tmux auto start

--- tmux/tmux.plugin.zsh
+++ tmux/tmux.plugin.zsh
@@ -3,7 +3,3 @@ local root=${${(%):-%x}:A:h}
 if [[ ! -e "$HOME/.tmux.conf" ]]; then
   ln -s $root/tmux.conf $HOME/.tmux.conf
 fi
-
-if [ -z "$TMUX" ]; then
-  $root/helpers/open_session.sh $PWD
-fi
