784129311deff83e192a04b5cfb255e0ce35cf11
Gigitsu <gigitsu.23@gmail.com>
2023-04-23 12:05:27 +0200

    Add docke and docker-compsoe plugin

---
+++ docker-compose/docker-compose.plugin.zsh
@@ -0,0 +1,23 @@
+# support Compose v2 as docker CLI plugin
+(( ${+commands[docker-compose]} )) && dccmd='docker-compose' || dccmd='docker compose'
+
+alias dco="$dccmd"
+alias dcob="$dccmd build"
+alias dcoe="$dccmd exec"
+alias dcops="$dccmd ps"
+alias dcorestart="$dccmd restart"
+alias dcorm="$dccmd rm"
+alias dcor="$dccmd run"
+alias dcostop="$dccmd stop"
+alias dcoup="$dccmd up"
+alias dcoupb="$dccmd up --build"
+alias dcoupd="$dccmd up -d"
+alias dcoupdb="$dccmd up -d --build"
+alias dcodn="$dccmd down"
+alias dcol="$dccmd logs"
+alias dcolf="$dccmd logs -f"
+alias dcopull="$dccmd pull"
+alias dcostart="$dccmd start"
+alias dcok="$dccmd kill"
+
+unset dccmd
