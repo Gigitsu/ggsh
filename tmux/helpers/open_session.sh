70b3d94e0e2435e4d70e885fdcd8f4cc4becd1f6
Gigitsu <gigitsu.23@gmail.com>
2023-04-30 13:15:59 +0200

    Add session init autoload script

--- tmux/helpers/open_session.sh
+++ tmux/helpers/open_session.sh
@@ -2,20 +2,26 @@
 
 session_exists() {
   tmux has-session -t "=$1"
+  echo $?
 }
 
 open_session() {
   local session_path=$1
   local session_name="$(basename "$session_path" | tr . -)"
+  local initialized=$(session_exists $session_name)
 
-  if [ -z "$TMUX" ]; then
-    tmux new-session -As "$session_name"
+  (TMUX='' tmux new-session -Ad -s "$session_name" -c $session_path)
+
+  if [[ $initialized != 0 && -f "${session_path}/.session_init.sh" ]]; then
+    (SESSION_NAME=$session_name source "${session_path}/.session_init.sh" $session_name)
+  fi
+
+  if [[ -z "$TMUX" ]]; then
+    tmux attach-session -t "$session_name"
   else
-    if ! session_exists "$session_name"; then
-      (TMUX='' tmux new-session -Ad -s "$session_name" -c $session_path)
-    fi
     tmux switch-client -t "$session_name"
   fi
 }
 
 open_session "$@"
+
